f85d5b88655032785dce89f86bdec48c68acd2fe
Joe D'Amore <joe@joedamore.me>
2026-07-12 15:58:54 -0400

    Install direnv

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ fi
 # this installs a package from fedora repos
 
 # Install utilities, etc.
-dnf5 -y install bluez kitty fastfetch zsh
+dnf5 -y install bluez kitty fastfetch zsh direnv
 
 # Install dev tools (TODO: Move these to special "Developer" edition or similar)
 dnf5 -y install ansible
